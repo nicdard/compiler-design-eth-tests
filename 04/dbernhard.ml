4bbe42e9b4e7588e3b9202d6d6ed40242000a39c
dbernhard <dbernhard@ethz.ch>
2021-11-14 12:44:49 +0100

    hw(4): more tests on while, indexing arrays, globals and using a library function

--- 04/dbernhard.ml
+++ 04/dbernhard.ml
@@ -3,7 +3,13 @@ open Assert
 let prefix = "./compiler-design-eth-tests/04/dbernhard/"
 
 let simple_tests = [
-  (prefix ^ "simple_while.oat", "", "10");
+  (prefix ^ "simple_while.oat", "", "10")
+  ; (prefix ^ "array_indexing.oat", "", "205") (* various indices *)
+  ; (prefix ^ "length.oat", "asdf", "4") (* returns length of the first argument *)
+  ; (prefix ^ "arr_of_string.oat", "abc", "98") (* returns second character as integer *)
+  ; (prefix ^ "simple_global_update.oat", "", "11")
+  ; (prefix ^ "simple_global_update2.oat", "", "11") (* update of a global variable *)
+  ; (prefix ^ "advanced_while.oat", "", "5")
 ]
 
 let dbernhard_tests = (Gradedtests.executed_oat_file simple_tests)
