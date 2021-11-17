d302833ebd95d7388a10cfc69e8b14daaf7d9b43
Nicola Dardanis <nicdard@gmail.com>
2021-11-17 10:10:36 +0100

    hw(4): Add easy tests on expressions and statements

--- 04/sharedtests.ml
+++ 04/sharedtests.ml
@@ -1,6 +1,6 @@
 open Assert
 
-
 let shared_suite : suite = 
-  [ Test ("Dbernhard tests", Dbernhard.dbernhard_tests);
+  [ Test ("Dbernhard tests", Dbernhard.dbernhard_tests)
+  ; Test ("Nicdard tests", Nicdard.nicdard_tests)
   ]
