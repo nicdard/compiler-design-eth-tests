738cd1391c228b097e20550174125d72415d0865
thbwd <hello@tbw.design>
2021-11-30 16:12:35 +0100

    Fix prefix

--- 05/thbwd.ml
+++ 05/thbwd.ml
@@ -5,18 +5,18 @@ let prefix = Test_config.global_prefix ^ "./compiler-design-eth-tests/05/thbwd/"
 
 let tests = (
   executed_oat_file [
-    ("thbwd/compile_struct_order.oat", "", "123412340");
-    ("thbwd/polymorphic_array.oat", "", "1230");
+    (prefix ^ "compile_struct_order.oat", "", "123412340");
+    (prefix ^ "polymorphic_array.oat", "", "1230");
   ] @
   typecheck_file_error [
-    "thbwd/dead_code1.oat";
-    "thbwd/dead_code2.oat";
-    "thbwd/dead_code3.oat";
-    "thbwd/assign_global_function.oat";
-    "thbwd/array_init_shadowing.oat";
+    prefix ^ "dead_code1.oat";
+    prefix ^ "dead_code2.oat";
+    prefix ^ "dead_code3.oat";
+    prefix ^ "assign_global_function.oat";
+    prefix ^ "array_init_shadowing.oat";
   ] @
   typecheck_file_correct [
-    "thbwd/undead_code1.oat";
-    "thbwd/undead_code2.oat";
+    prefix ^ "undead_code1.oat";
+    prefix ^ "undead_code2.oat";
   ]
 )
