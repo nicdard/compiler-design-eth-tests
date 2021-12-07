99ec390422b57ae34aa5002ceecb09ff4125bbed
Nicola Dardanis <nicdard@gmail.com>
2021-12-07 18:46:53 +0100

    Revert "Add tests"

--- 05/sharedtests.ml
+++ 05/sharedtests.ml
@@ -3,5 +3,4 @@ open Assert
 let shared_suite : suite =
   [ Test ("Dbernhard tests", Dbernhard.dbernhard_tests)
   ; Test ("Nicdard tests", Nicdard.nicdard_tests)
-  ; Test ("thbwd tests", Thbwd.tests)
   ]
