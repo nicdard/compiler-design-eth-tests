d2ffe01f261dc38af266634e423b544149378304
thbwd <hello@tbw.design>
2021-12-07 18:22:03 +0100

    Resolve merge conflict

--- 05/sharedtests.ml
+++ 05/sharedtests.ml
@@ -2,5 +2,6 @@ open Assert
 
 let shared_suite : suite =
   [ Test ("Dbernhard tests", Dbernhard.dbernhard_tests)
+  ; Test ("Nicdard tests", Nicdard.nicdard_tests)
   ; Test ("thbwd tests", Thbwd.tests)
   ]
